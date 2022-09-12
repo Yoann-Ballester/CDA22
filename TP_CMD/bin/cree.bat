58c6d63c0030e25a21383a30f960cf0238bc9811
Yoann-Ballester <ballester.yoann@gmail.com>
2022-09-12 18:47:30 +0200

    TP_CMD

---
+++ TP_CMD/bin/cree.bat
@@ -0,0 +1,9 @@
+
+mkdir %TP_HOME%\repertoire1\r5 %TP_HOME%\r2 %TP_HOME%\r3 %TP_HOME%\r4\r6 %TP_HOME%\r4\r7 %TP_HOME%\r4\r8
+cd %TP_HOME%\repertoire1 
+type nul > fichier1.txt
+type nul > f2.txt
+cd %TP_HOME%\r4\r8
+type nul > f3.txt
+
+
---
+++ TP_CMD/bin/supprime.bat
@@ -0,0 +1,2 @@
+
+rmdir %TP_HOME%\repertoire1 %TP_HOME%\r4 %TP_HOME%\r3 %TP_HOME%\r2 /s /q
